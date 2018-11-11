acc9b15b2c5809e9522d1db84f633f67e55d807e
coene035 <coene035@umn.edu>
2018-11-10 20:28:52 -0600

    Implement all functionality

--- release/client.c
+++ release/client.c
@@ -27,7 +27,6 @@ void main(int argc, char * argv[]) {
 		print_prompt(username);
 	}
 	/* -------------- YOUR CODE STARTS HERE -----------------------------------*/
-//	print_prompt(username);
 	fcntl(STDIN_FILENO, F_SETFL, fcntl(0, F_GETFL)| O_NONBLOCK); //This line makes stdin non-blocking
 	fcntl(pipe_user_reading_from_server[0], F_SETFL, fcntl(0, F_GETFL)| O_NONBLOCK);
 	while(1) {
@@ -37,13 +36,8 @@ void main(int argc, char * argv[]) {
 
 
 		// Poll stdin (input from the terminal) and send it to server (child process) via pipe
-//		print_prompt(username);
 		if(read(STDIN_FILENO, tx_buf, MAX_MSG) > 0) {
-//			close(pipe_user_writing_to_server[0]);
 			write(pipe_user_writing_to_server[1], tx_buf, MAX_MSG);
-			printf("Client wrote to the child: %s\n", tx_buf);
-//			close(pipe_user_writing_to_server[1]);
-//			open(pipe_user_writing_to_server[0], O_RDONLY);
 			print_prompt(username);
 
 			// poll pipe retrieved and print it to sdiout - Retrieve information from the child process
@@ -51,8 +45,12 @@ void main(int argc, char * argv[]) {
 		//Pread allows us to force the read to occur from byte zero.
 		if(read(pipe_user_reading_from_server[0], rx_buf, MAX_MSG) > 0) {
 			close(pipe_user_reading_from_server[1]);
-			if (rx_buf != "\0") {
-				printf("Client received from the server: %s\n", rx_buf);
+		 	if (rx_buf != "\0") {
+		 	if (strcmp(rx_buf,"User already exists\n") == 0) {
+					printf("%s", rx_buf);
+		 			exit(0);
+				}
+				printf("%s", rx_buf);
 				print_prompt(username);
 			}
 			open(pipe_user_reading_from_server[1], O_WRONLY);
--- release/server.c
+++ release/server.c
@@ -55,15 +55,14 @@ int list_users(int idx, USER * user_list)
     // if every slot is empty, print "<no users>""
 	} else {
 		s--;
-		strncpy(s, "\0", 1); //Place Null character at end of string buffer.
+		strncpy(s, "\n\0", 2); //Place Null character at end of string buffer.
 	}
 
 	if(idx < 0) {
 		printf(buf);
-		printf("\n");
 	} else {
 		/* write to the given pipe fd */
-		if (write(user_list[idx].m_fd_to_server, buf, strlen(buf) + 1) < 0)
+		if (write(user_list[idx].m_fd_to_user, buf, strlen(buf) + 1) < 0)
 			perror("failed to write user_list to server shell");
 	}
 
@@ -95,22 +94,18 @@ int add_user(int idx, USER * user_list, int pid, char * user_id, int pipe_to_use
 void kill_user(int child_idx, USER * user_list) {
 	// kill a user (specified by idx) by using the systemcall kill()
 	USER dude = user_list[child_idx];
-	printf("Killing ID: %s\n", dude.m_user_id);
 	int pid_to_kill = dude.m_pid;
 	char buf[10];
-	sprintf(buf, "%d", pid_to_kill);
 	if (kill(pid_to_kill, SIGKILL) < 0) {
 		printf("Failed to kill the selected USER\n");
 	}; // The system call to kill the user client
 	int status;
 	waitpid(pid_to_kill, &status, 0);
-	printf("Killed client process: %d\n", pid_to_kill);
 	if (kill(pid_to_kill-1, SIGKILL) < 0) {
 		printf("Failed to kill the child proecess\n");
 	}; // The system call to kill the child process
 	int status2;
 	waitpid(pid_to_kill-1, &status, 0);
-	printf("Killed child process: %d\n", pid_to_kill-1);
 }
 /*
  * Perform cleanup actions after the user has been killed
@@ -118,7 +113,6 @@ void kill_user(int child_idx, USER * user_list) {
 void cleanup_user(int child_idx, USER * user_list)
 {
 	USER user_cleaning = user_list[child_idx];
-	printf("%s\n", user_list[child_idx].m_user_id);
 	// m_pid should be set back to -1
 	user_cleaning.m_pid = -1;
 	// m_user_id should be set to zero, using memset()
@@ -147,16 +141,23 @@ void kick_user(int user_idx, USER * user_list) {
 /*
  * broadcast message to all users
  */
-int broadcast_msg(USER * user_list, char *buf, char *sender)
+int broadcast_msg(USER * user_list, char *buf, int idx)
 {
 	//iterate over the user_list and if a slot is full, and the user is not the sender itself,
 	//then send the message to that user
 	//return zero on success
   for (int i = 0; i < MAX_USER; i++) {
-    if (user_list[i].m_status == SLOT_EMPTY) { // Reached the last user
+    if (user_list[i].m_status == SLOT_EMPTY || idx == i) { // Reached the last user
       continue;
     }
     else {
+      write(user_list[i].m_fd_to_user, "\n", 1);
+      if (idx != -1) {
+        write(user_list[i].m_fd_to_user, user_list[idx].m_user_id, strlen(user_list[idx].m_user_id)); // Cap username length at MAX_MSG
+        write(user_list[i].m_fd_to_user, ": ", 2);
+      }
+      else
+        write(user_list[i].m_fd_to_user, "admin: ", 7);
       write(user_list[i].m_fd_to_user, buf, MAX_CMD_LENGTH);
     }
   }
@@ -249,13 +250,16 @@ int extract_text(char *buf, char * text)
 /*
  * send personal message
  */
-void send_p2p_msg(int idx, USER * user_list, char *buf)
-{
-
-	// get the target user by name using extract_name() function
-	// find the user id using find_user_index()
-	// if user not found, write back to the original user "User not found", using the write()function on pipes.
-	// if the user is found then write the message that the user wants to send to that user.
+void send_p2p_msg(int recipient, USER * user_list, char *buf, int sender) {
+    // Recipient is checked to be valid before this function is called.
+    write(user_list[recipient].m_fd_to_user, "\n", 1);
+    write(user_list[recipient].m_fd_to_user, user_list[sender].m_user_id, strlen(user_list[sender].m_user_id)); // Cap username length at MAX_MSG
+    write(user_list[recipient].m_fd_to_user, " whispers: ", 11);
+    if (strlen(buf) < MAX_CMD_LENGTH)
+      write(user_list[recipient].m_fd_to_user, buf, strlen(buf));
+    else
+    write(user_list[recipient].m_fd_to_user, buf, MAX_CMD_LENGTH);
+    write(user_list[recipient].m_fd_to_user, "\n", 1);
 }
 
 /*
@@ -292,9 +296,7 @@ void parse_message(char* cmd_buf, USER user_list[], int idx){
 		while (pch != NULL)
 		{
 			if(pch != "\n") {
-//				printf("%s\n", pch);
 				arg_array[length_of_cmd_array] = pch;
-//				printf("%s\n", arg_array[length_of_cmd_array]);
 				pch = strtok(NULL, " \n"); //Trims all spaces and newlines, should now have an arg array in pch.
 				if(pch != "") {
 					char *tmp = (char *) malloc(strlen(arg_array[length_of_cmd_array]));
@@ -307,16 +309,10 @@ void parse_message(char* cmd_buf, USER user_list[], int idx){
 
 		//Sanity check in case of array only being one word long.
 		if(length_of_cmd_array == 1){
-//			printf("length of cmd array was one\n");
 			char tmp[MAX_CMD_LENGTH] = {"\0"};
 			strncpy(tmp, arg_array[0], strlen(arg_array[0])-1); //Trims new lines
 			arg_array[0] = tmp;
 		}
-		/**This block should trim any excess char, specifically, the trailing newline.
-		 * Make sure to take care of excess malloc **/
-		// for(int i = 0; i < length_of_cmd_array; i++) {
-		// 	printf("Position %d: %s\n", i, arg_array[i]);
-		// }
 
 	// This value is the value that indicates what position in the commands array in util.c the command was
 		// found at
@@ -326,35 +322,54 @@ void parse_message(char* cmd_buf, USER user_list[], int idx){
     switch (found){
   		case LIST:
         /** List **/
-  			list_users(-1, user_list); //See definition of list_users, must be called with -1 to make sense.
+  			list_users(idx, user_list); //See definition of list_users, must be called with -1 to make sense.
         break;
    		case KICK:
         /** Kick **/
-        child_idx = find_user_index(user_list, arg_array[1]);
-        if (child_idx != -1) {
-   			  kick_user(child_idx, user_list);
+        if (idx == -1) {
+          child_idx = find_user_index(user_list, arg_array[1]);
+          if (child_idx != -1) {
+     			  kick_user(child_idx, user_list);
+          } else {
+            printf("User %s does not exist\n", arg_array[1]);
+          }
         } else {
-          printf("User %s does not exist", arg_array[1]);
+          broadcast_msg(user_list, cmd_buf, idx);
         }
          break;
    		case P2P:
          /** P2P **/
+         if(idx != -1) {
+           int recipient = find_user_index(user_list, arg_array[1]);
+           if (recipient != -1)
+            send_p2p_msg(recipient, user_list, arg_array[2], idx);
+           else {
+             write(user_list[idx].m_fd_to_user, "User does not exist\n", strlen("User does not exist\n"));
+          }
+         } else {
+           broadcast_msg(user_list, cmd_buf, idx);
+         }
          break;
    		case SEG:
          /** Seg **/
          break;
    		case EXIT:
          /** Exit **/
-         cleanup_users(user_list);
-         exit(0);
+        if(idx == -1) {
+          cleanup_users(user_list);
+          exit(0);
+        } else {
+          kick_user(idx, user_list);
+        }
          break;
    		default:
          /** No Command Entered **/
-        broadcast_msg(user_list, cmd_buf, "admin");
+        broadcast_msg(user_list, cmd_buf, idx);
 	 	}
 		//Should always print the admin prompt before exiting to get ready for the next input
     cmd_buf = "\0";
-		print_prompt("admin");
+    if (idx < 0)
+		  print_prompt("admin");
 }
 
 /* ---------------------Start of the Main function ----------------------------------------------*/
@@ -400,8 +415,7 @@ int main(int argc, char * argv[])
     for (int i = 0; i < MAX_USER; i++) {
       if(user_list[i].m_status != SLOT_EMPTY) {
     		if(read(user_list[i].m_fd_to_server, rx_buf, MAX_MSG) > 0) {
-    			printf("Server received from child: %s\n", rx_buf);
-    			print_prompt("admin");
+          parse_message(rx_buf, user_list, i);
     		}
       }
     }
@@ -413,7 +427,7 @@ int main(int argc, char * argv[])
 		/** If we find a new connection, then we have a server and child process to take care of.**/
 		if(get_connection(user_id, pipe_client_reading_from_child, pipe_client_writing_to_child) != -1) {
       if(find_user_index(user_list, user_id) >= 0){
-        printf("User %s already exists", user_id);
+        write(pipe_client_reading_from_child[1], "User already exists\n", strlen("User already exists\n"));
       } else {
   			printf("\nAdded a new user!\n");
         print_prompt("admin");
@@ -466,58 +480,14 @@ int main(int argc, char * argv[])
   					char tx_buf[MAX_MSG];
 
   					if (read(pipe_client_writing_to_child[0], tx_buf, MAX_MSG) > 0){
-  						printf("Child received from client: %s\n", tx_buf);
   						//Write the message received from the client, that is now in the child, to the server
   						write(pipe_SERVER_reading_from_child[1], tx_buf, MAX_MSG);
-  						printf("Child wrote to the server\n");
   //						open(pipe_SERVER_reading_from_child[0], O_RDONLY);
   					}
   				}
   			}
-  			/** The server process at one time was part of the fork and this was wrong, now we want it to be the
-  			 * identity of main and to have many children, as is the setup now.
-  			 */
-
-  				// Server process:
-  //			if(pid>0){
-  //				while(1) {
-  //					usleep(SLEEP_TIME);
-  //					char tx_buf[MAX_MSG];
-  //					char rx_buf[MAX_MSG];
-  //					char stdin[MAX_MSG] = {"\0"};
-  //					fcntl(0, F_SETFL, fcntl(0, F_GETFL)| O_NONBLOCK); //Makes reads of stdin non-blocking
-  //					fcntl(pipe_SERVER_reading_from_child[0], F_SETFL, fcntl(0, F_GETFL)| O_NONBLOCK); //Makes reads of stdin non-blocking
-  //					// Add a new user information into an empty slot
-  //					// poll child processes and handle user commands
-  ////					close(pipe_SERVER_reading_from_child[1]);
-  //					if(read(pipe_SERVER_reading_from_child[0], rx_buf, MAX_MSG) > 0) {
-  //						printf("Server received from child: %s\n", rx_buf);
-  ////						open(pipe_SERVER_reading_from_child[1], O_WRONLY);
-  //						print_prompt("admin");
-  //					}
-  //
-  //					// Poll stdin (input from the terminal) and handle admnistrative command
-  //					/** In this block, we are polling the input **/
-  //
-  //					if(read(STDIN_FILENO,stdin, MAX_MSG) > 0) { //If there is data in stdin, put it into a buffer
-  //						printf("%s\n", stdin);
-  //						pollSTDIN(stdin, user_list, pipe_SERVER_writing_to_child, pipe_SERVER_reading_from_child);
-  //						/** This block executes if told to broadcast **/
-  //					}
-  //				}
-  //			}
-  //			else{
-  //				perror("pipe failed\n");
-  //				exit(-1);
-  //			}
   		}
     }
-
-  		/** If we do not have a new user, then these actions will take place **/
-
-
-  //		pollSTDIN(cmd_buf, user_list, pipe_SERVER_writing_to_child ,pipe_SERVER_reading_from_child);
-
   		/* ------------------------YOUR CODE FOR MAIN--------------------------------*/
 	}
 }
